1538b79295cc5b6a3d9fcebafafae03688f82118
Pawel Ostrzolek <ostrzolekpawel@gmail.com>
2025-12-11 13:08:58 +0100

    view data provider

---
+++ Assets/_GamestureAssignment/Scripts/CollectableDisplayer/IViewDataProvider.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using GamestureAssignment.Configs;
+using Osiris.Configs;
+
+namespace GamestureAssignment.CollectableDisplayer
+{
+    public interface IViewDataProvider<in TData, TViewData>
+    {
+        TViewData GetViewData(TData data);
+        UniTask<TViewData> GetViewDataAsync(TData data);
+    }
+
+    public class CollectableViewDataProvider : IViewDataProvider<CollectableInfo, CollectableViewData>
+    {
+        private readonly IConfig<CollectableInfo, CollectableViewData> _config;
+
+        public CollectableViewDataProvider(IConfig<CollectableInfo, CollectableViewData> config)
+        {
+            _config = config;
+        }
+
+        public CollectableViewData GetViewData(CollectableInfo data)
+        {
+            return _config.GetData(data);
+        }
+
+        public UniTask<CollectableViewData> GetViewDataAsync(CollectableInfo data)
+        {
+            return UniTask.FromResult(_config.GetData(data));
+        }
+    }
+
+    public enum CollectableViewType // TODO use it to load different views?
+    {
+        DailyReward,
+        HUD
+    }
+
+    public interface IViewDataProviderFactory<in TType, out IViewDataProvider>
+    {
+        IViewDataProvider GetViewProvider(TType type);
+    }
+
+    public class CollectableViewDataProviderFactory : IViewDataProviderFactory<CollectableViewType, IViewDataProvider<CollectableInfo, CollectableViewData>>
+    {
+        private readonly CollectableViewDataProvider _dailyRewardsViewProvider;
+        private readonly Dictionary<CollectableViewType, IViewDataProvider<CollectableInfo, CollectableViewData>> _map;
+
+        public CollectableViewDataProviderFactory(IConfig<CollectableInfo, CollectableViewData> dailyRewardsConfig)
+        {
+            _map = new Dictionary<CollectableViewType, IViewDataProvider<CollectableInfo, CollectableViewData>>
+            {
+                [CollectableViewType.DailyReward] = new CollectableViewDataProvider(dailyRewardsConfig)
+            };
+        }
+
+        // get different views
+
+        public IViewDataProvider<CollectableInfo, CollectableViewData> GetViewProvider(CollectableViewType type)
+        {
+            if (_map.TryGetValue(type, out var provider))
+            {
+                return provider;
+            }
+
+            throw new Exception($"No provider for type [{type}]");
+        }
+    }
+}
